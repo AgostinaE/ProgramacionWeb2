c178cd6c5093695e80fc5e36e9a85a60e80c717b
AgostinaE <agos.espin@gmail.com>
2022-06-08 18:37:23 -0300

    a ver ahora

--- validation.js
+++ validation.js
@@ -2,25 +2,28 @@ let nombre = document.getElementById("name");
 let email = document.getElementById("email");
 let subject = document.getElementById("subject");
 let error = document.getElementById("error")
+let correcto = document.getElementById("msjcorrecto")
 error.style.margin = '20px';
-error.style.color = '#343840'
+error.style.color = '#343840';
+correcto.style.margin = '20px';
+correcto.style.color = '#343840';
 function enviar() {
     let msjError = [];
+    let msjCorrecto = [];
     if (nombre.value === null || nombre.value === '') {
         msjError.push('Falta tu nombre');
-    }else{
-        msjError.push('Su nombre es '+ nombre);
     }
     if (email.value === null || email.value === '') {
          msjError.push('Falta tu direccion de email');
-    }else{
-        msjError.push('Su email es '+ email);
     }
     if (subject.value === null || subject.value === '') {
          msjError.push('Falta tu asunto');
-    }else{
-        msjError.push('Su mensaje es '+ subject);
     }
+    if (msjError.value === null
+        {
+            msjCorrecto.push('El usuario de nombre '+ nombre + ' de correo '+ email + ' consulta sobre '+ subject);
+        })
     error.innerHTML = msjError.join(',')
+    correcto.innerHTML = msjCorrecto.join(',')
     return false;
 }
