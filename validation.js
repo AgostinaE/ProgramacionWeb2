97773cfb4bba5197489f06c1b592dbab93f2e6ca
AgostinaE <agos.espin@gmail.com>
2022-06-08 23:28:04 -0300

    elimino lo hecho

--- validation.js
+++ validation.js
@@ -2,30 +2,19 @@ let nombre = document.getElementById("name");
 let email = document.getElementById("email");
 let subject = document.getElementById("subject");
 let error = document.getElementById("error")
-let correcto = document.getElementById("msjcorrecto")
 error.style.margin = '20px';
-error.style.color = '#343840';
-correcto.style.margin = '20px';
-correcto.style.color = '#343840';
+error.style.color = '#343840'
 function enviar() {
     let msjError = [];
     if (nombre.value === null || nombre.value === '') {
-        msjError.push('Falta tu nombre');
+        msjError.push('falta tu nombre');
     }
     if (email.value === null || email.value === '') {
-         msjError.push('Falta tu direccion de email');
+         msjError.push('falta tu direccion de email');
     }
     if (subject.value === null || subject.value === '') {
-         msjError.push('Falta tu asunto');
+         msjError.push('falta tu asunto');
     }
     error.innerHTML = msjError.join(',')
     return false;
-}
-function correcto(enviar()){
-    let msjCorrecto = [];
-    if (enviar === false)
-        {
-            msjCorrecto.push('El usuario de nombre '+ nombre + ' de correo '+ email + ' consulta sobre '+ subject);
-        }
-        correcto.innerHTML = msjCorrecto.join(',');
 }
